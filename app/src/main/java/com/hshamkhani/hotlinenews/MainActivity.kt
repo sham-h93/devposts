537ab7ff83e9fa3b6d7984e214f262c769822f95
H-Shamkhani <shamkhanihossein93@gmail.com>
2025-07-22 13:21:43 +0330

    initial commit

---
+++ app/src/main/java/com/hshamkhani/hotlinenews/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.hshamkhani.hotlinenews
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.hshamkhani.hotlinenews.ui.theme.HotlinenewsTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            HotlinenewsTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+
+                }
+            }
+        }
+    }
+}
