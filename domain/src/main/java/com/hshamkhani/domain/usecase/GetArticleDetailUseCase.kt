7f80bbb503524cd95391eadd3f6ba6cd0bd57a55
H-Shamkhani <shamkhanihossein93@gmail.com>
2025-07-23 20:16:09 +0330

    Change coroutine dispatcher in GetArticleDetailUseCase to io

--- domain/src/main/java/com/hshamkhani/domain/usecase/GetArticleDetailUseCase.kt
+++ domain/src/main/java/com/hshamkhani/domain/usecase/GetArticleDetailUseCase.kt
@@ -1,12 +1,20 @@
 package com.hshamkhani.domain.usecase
 
+import com.hshamkhani.core.Dispatcher
+import com.hshamkhani.core.Dispatchers
 import com.hshamkhani.domain.repository.NewsRepository
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 class GetArticleDetailUseCase
     @Inject
     constructor(
         private val newsRepository: NewsRepository,
+        @Dispatcher(Dispatchers.IO) private val dispatcher: CoroutineDispatcher,
     ) {
-        suspend operator fun invoke(id: String) = newsRepository.getArticleById(id)
+        suspend operator fun invoke(id: String) =
+            withContext(dispatcher) {
+                newsRepository.getArticleById(id = id)
+            }
     }
---
+++ libraries/core/src/main/java/com/hshamkhani/core/Dispatchers.kt
@@ -0,0 +1,18 @@
+package com.hshamkhani.core
+
+import javax.inject.Qualifier
+
+/**
+ * A custom qualifier annotation for dependency injection to specify the type of dispatcher.
+ * */
+
+@Qualifier
+@Retention(AnnotationRetention.BINARY)
+annotation class Dispatcher(
+    val dispatchers: Dispatchers,
+)
+
+enum class Dispatchers {
+    Default,
+    IO,
+}
